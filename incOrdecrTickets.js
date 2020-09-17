847c558e5530aec449e1140f5ccf545aae438951
Michal Luszczek <michaluszczek@gmail.com>
2020-09-17 23:30:34 +0200

    newNamesForm

--- incOrdecrTickets.js
+++ incOrdecrTickets.js
@@ -3,7 +3,7 @@ $("#add_btn").on('click', function(){
     if(amount < 8) {
         amount++;
         $("#ticketAmnt").val(amount); 
-        $('#fields').prepend("<input type= 'text' name='name' placeholder='Name' id =fn_" + amount + ">");
+        $('#fields').prepend("<form class = 'double_field' id = 'f1'><input type= 'text' class = 'field' name='first_name' placeholder='First name' id =fn_" + amount + "><input type= 'text' class = 'field' name='last_name' placeholder='Last name' id =ln_" + amount + "></form>");
                 
     }   
   });
@@ -16,15 +16,3 @@ $("#rem_btn").on('click', function(){
         $("#ticketAmnt").val(amount);
     }
 });
-  
-function appendNames() {
-    var icon = "<z class='fa fa-user'></i>";
-    //$('#fields').append(icon);
-    $('#fields').prepend($new_field);
-  }
-
-function removeNames() {
-
-    var new_field = "<input type='text' name='name' placeholder='FirstName' />"; 
-    $(new_field).remove();
-    }
